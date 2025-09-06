077d022cab80d0a1ee282a4cacf16eb258e3f34c
Syamchand Challa <syamchandchalla22@gmail.com>
2025-09-06 21:26:52 +0530

    Added .env and updated frontend API config

--- invoice-frontend/src/service/InvoiceService.js
+++ invoice-frontend/src/service/InvoiceService.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const getInvoices = async () => {
+  const response = await axios.get(`${API_BASE_URL}/invoices`);
+  return response.data;
+};
+
 export const getAllInvoices = (baseURL, token) => {
   return axios.get(`${baseURL}/invoices`, {
     headers: {
