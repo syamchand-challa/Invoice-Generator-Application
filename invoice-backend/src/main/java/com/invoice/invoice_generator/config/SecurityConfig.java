27fee2eb144ec191661061980c8ea227bd985c07
Syamchand Challa <syamchandchalla22@gmail.com>
2025-09-07 14:50:32 +0530

    fix: add Netlify domain to CORS allowed origins

--- invoice-backend/src/main/java/com/invoice/invoice_generator/config/SecurityConfig.java
+++ invoice-backend/src/main/java/com/invoice/invoice_generator/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://invoice-generator-appllication.netlify.app" ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
